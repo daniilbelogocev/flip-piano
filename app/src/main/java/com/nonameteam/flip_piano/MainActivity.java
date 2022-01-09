a9278b1820c535fd9937f33d866583ee15d78809
Danila Essence <daniltarasov095@mail.ru>
2022-01-09 14:19:03 +0300

    metod frameTimer

--- app/src/main/java/com/nonameteam/flip_piano/MainActivity.java
+++ app/src/main/java/com/nonameteam/flip_piano/MainActivity.java
@@ -2,6 +2,7 @@ package com.nonameteam.flip_piano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,17 +12,40 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    private List<PianoPiece> pieces;
+    public List<PianoPiece> pieces;
     private Timer timer;
+    private Timer frameTimer;
+    private static MainActivity context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.context = this;
         RotationService.initialize(this);
         this.pieces = new ArrayList<>();
         this.timer = new Timer();
         this.timer.schedule(new PiecesCreatorTask(), 3000, 5000);
+
+
+        frameTimer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < MainActivity.context.pieces.size(); i++) {
+                            float y = MainActivity.context.pieces.get(i).view.getTranslationY();
+                            MainActivity.context.pieces.get(i).view.setTranslationY(y + 2);
+                        }
+
+                    }
+                });
+            }
+        };
+        frameTimer.schedule(task, 0, 50);
     }
 }
 
