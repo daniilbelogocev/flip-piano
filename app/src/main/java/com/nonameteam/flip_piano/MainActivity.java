2f460378b0ffd56b3fbc6d7b9bda5ee2509571e7
AndrewLevada <levada.andrew@gmail.com>
2022-01-09 11:42:56 +0300

    Added simple pieces xml

--- app/src/main/java/com/nonameteam/flip_piano/MainActivity.java
+++ app/src/main/java/com/nonameteam/flip_piano/MainActivity.java
@@ -3,12 +3,23 @@ package com.nonameteam.flip_piano;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<PianoPiece> pieces;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.pieces = new ArrayList<>();
     }
+}
+
+class PianoPiece {
+    public View view;
+    public RotationType lane;
 }
