852bc05cea9e7d2622aef88a8bb9026c690fcd80
AndrewLevada <levada.andrew@gmail.com>
2022-01-09 12:23:05 +0300

    Added timer for blocks creation

--- app/src/main/java/com/nonameteam/flip_piano/MainActivity.java
+++ app/src/main/java/com/nonameteam/flip_piano/MainActivity.java
@@ -7,15 +7,27 @@ import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private List<PianoPiece> pieces;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.pieces = new ArrayList<>();
+        this.timer = new Timer();
+        this.timer.schedule(new PiecesCreatorTask(), 3000, 5000);
+    }
+}
+
+class PiecesCreatorTask extends TimerTask {
+    @Override
+    public void run() {
+
     }
 }
 
