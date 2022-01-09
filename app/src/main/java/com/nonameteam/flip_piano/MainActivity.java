048969fc239a180a0aa6c891bb5e945cf69c6ed7
AndrewLevada <levada.andrew@gmail.com>
2022-01-09 16:08:53 +0300

    Added colision logic

--- app/src/main/java/com/nonameteam/flip_piano/MainActivity.java
+++ app/src/main/java/com/nonameteam/flip_piano/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private Timer frameTimer;
     private static MainActivity context;
     public ViewGroup piecesParent;
+    public TextView counterView;
+    public int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         RotationService.initialize(this);
         this.pieces = new ArrayList<>();
         this.piecesParent = findViewById(R.id.pieces_parent);
+        this.counterView = findViewById(R.id.count);
 
         this.timer = new Timer();
         this.timer.schedule(new TimerTask() {
@@ -54,8 +58,22 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 runOnUiThread(() -> {
                     for (int i = 0; i < MainActivity.context.pieces.size(); i++) {
-                        float y = MainActivity.context.pieces.get(i).view.getTranslationY();
-                        MainActivity.context.pieces.get(i).view.setTranslationY(y + 12);
+                        if (isHiddenFromScreen(MainActivity.context.pieces.get(i))) {
+                            MainActivity.context.piecesParent.removeView(MainActivity.context.pieces.get(i).view);
+                            MainActivity.context.pieces.remove(i);
+                            break;
+                        }
+                    }
+
+                    for (int i = 0; i < MainActivity.context.pieces.size(); i++) {
+                        PianoPiece p = MainActivity.context.pieces.get(i);
+                        float y = p.view.getTranslationY();
+                        p.view.setTranslationY(y + 12);
+
+                        if (isInBottom(p) && isRotationCorrect(p)) {
+                            MainActivity.context.score++;
+                            MainActivity.context.counterView.setText(String.valueOf(MainActivity.context.score));
+                        }
                     }
                 });
             }
@@ -69,10 +87,26 @@ public class MainActivity extends AppCompatActivity {
 
     private float getLaneOffset(RotationType l) {
         if (l == RotationType.middle) return 0f;
+        return (l == RotationType.left ? -1 : 1) * getScreenSize().x / 3f;
+    }
+
+    private boolean isInBottom(PianoPiece piece) {
+        return piece.view.getTranslationY() - 40 >= getScreenSize().y;
+    }
+
+    private boolean isHiddenFromScreen(PianoPiece piece) {
+        return piece.view.getTranslationY() - 80 >= getScreenSize().y;
+    }
+
+    private boolean isRotationCorrect(PianoPiece piece) {
+        return RotationService.getCurrentPhoneRotation() == piece.lane;
+    }
+
+    private Point getScreenSize() {
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        return (l == RotationType.left ? -1 : 1) * size.x / 3f;
+        return size;
     }
 }
 
